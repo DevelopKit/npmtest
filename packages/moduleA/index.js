51f881e2277549a66b5beff6f8f8a9c29862f41d
翟强 <zhaiq@weipaitang.com>
2023-12-04 12:04:27 +0800

    feat: init

---
+++ packages/moduleA/index.js
@@ -0,0 +1,4 @@
+import add from '@wwt/utils'
+export default function moduleA() {
+    return add(2+2);
+}
---
+++ packages/moduleB/index.js
@@ -0,0 +1,4 @@
+import add from '@wwt/utils'
+export default function moduleB() {
+    return add(1+1)
+}
