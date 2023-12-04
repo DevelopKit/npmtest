dec69fba484809f2b1d78e84106fa6f5e7b85228
翟强 <zhaiq@weipaitang.com>
2023-12-04 16:38:55 +0800

    feat: modify deps

--- packages/moduleA/index.js
+++ packages/moduleA/index.js
@@ -1,4 +1,4 @@
-import add from '@wwt/utils'
+import add from '@earjs/utils';
 export default function moduleA() {
     return add(2+2);
 }
--- packages/moduleB/index.js
+++ packages/moduleB/index.js
@@ -1,4 +1,4 @@
-import add from '@wwt/utils'
+import add from '@earjs/utils'
 export default function moduleB() {
     return add(1+1)
 }
